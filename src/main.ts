02833a04985f9c36840ddc4aa43ee2dd72039c9f
Ian-Stentz <ianstentz25@gmail.com>
2024-11-11 03:10:33 -0800

    Button and HTML fixed (kind of)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 // todo
 import "./style.css";
 
+const APP_NAME = "Ian's Geocoin";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = APP_NAME;
+app.innerHTML = APP_NAME;
+
 function alertButFunct() {
   alert("you clicked the button");
 }
@@ -8,3 +14,4 @@ function alertButFunct() {
 const alertButton = document.createElement("button");
 alertButton.innerHTML = "ALERT";
 alertButton.addEventListener("click", alertButFunct);
+app.append(alertButton);
