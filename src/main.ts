c9600cb7798d7138cbb602cdcd1e7f64ffd28dfb
Ian-Stentz <ianstentz25@gmail.com>
2024-11-11 02:13:50 -0800

    Button added, leaflet dependency added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+import "./style.css";
+
+function alertButFunct() {
+  alert("you clicked the button");
+}
+
+const alertButton = document.createElement("button");
+alertButton.innerHTML = "ALERT";
+alertButton.addEventListener("click", alertButFunct);
