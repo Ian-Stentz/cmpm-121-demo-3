7190bb7d229d2c2b4c17bbc36c13f1e8801b4574
Ian-Stentz <ianstentz25@gmail.com>
2024-11-12 11:02:47 -0800

    Split HTML into sectors, now title and ALERT are seperated

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
 import "./style.css";
 
-const APP_NAME = "Ian's Geocoin";
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Deterministic random number generator
+import luck from "./luck.ts";
+
+const APP_NAME = "Santa Cruz Geocoin";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const title = app.querySelector<HTMLDivElement>("#title")!;
+const header = app.querySelector<HTMLDivElement>("#header")!;
+const map = app.querySelector<HTMLDivElement>("#map")!;
+const footer = app.querySelector<HTMLDivElement>("#footer")!;
+
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+title.innerHTML = APP_NAME;
 
 function alertButFunct() {
   alert("you clicked the button");
@@ -13,4 +29,4 @@ function alertButFunct() {
 const alertButton = document.createElement("button");
 alertButton.innerHTML = "ALERT";
 alertButton.addEventListener("click", alertButFunct);
-app.append(alertButton);
+header.append(alertButton);
